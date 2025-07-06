002658fd24f6ca17fb889ee247bd2a9ff81b2d4e
T.Miura <mtaiga0213@icloud.com>
2025-07-06 15:03:16 +0900

    テストユーティリティの配置を修正してテストエラーを解決
    - test-utilsを__tests__外に移動してJestがテストファイルとして認識しないように修正
    - すべてのテストファイルのimportパスを更新
    - テストスイートが正常に実行されるように修正

--- ReMeet/__tests__/components/ThemedText.test.tsx
+++ ReMeet/__tests__/components/ThemedText.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '../utils/test-utils';
+import { render } from '../../test-utils/test-utils';
 import { ThemedText } from '../../components/ThemedText';
 
 describe('ThemedText', () => {
